d97cea582ee5e4d807936603db78d6f47af51d32
SOHAM  SAHA  ROY <81096719+infuman69@users.noreply.github.com>
2021-12-01 01:09:38 +0530

    Time: 4 ms (74.98%), Space: 9.4 MB (79.42%) - LeetHub

---
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int i ,j;
+       
+     for(auto& it :matrix)
+     {
+         int low = 0;
+         int high = it.size()-1;
+         
+         while(low<=high)
+         {    int mid = (low + high)/2;;
+             if(target == it[mid])
+             {
+                 return true;
+             }
+             else if(target<it[mid])
+             {
+                 high=mid-1;
+                }
+             else{
+                 low=mid+1;
+             }
+         }
+     }
+     return false;
+    }
+};
