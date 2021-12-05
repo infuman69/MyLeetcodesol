d8ecba184a07b83a99a5d7e8bf457de699aeb796
SOHAM  SAHA  ROY <81096719+infuman69@users.noreply.github.com>
2021-12-05 20:57:21 +0530

    Time: 568 ms (93.71%), Space: 180.1 MB (92.33%) - LeetHub

--- swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -10,30 +10,45 @@
  */
 class Solution {
 public:
+    int size(ListNode* head)
+    {
+        int count = 0;
+        while(head!=NULL)
+        {
+            count++;
+            head = head->next;
+            
+        }
+        return count;
+    }
     ListNode* swapNodes(ListNode* head, int k) 
-    {  vector<int> swappedlist;
-       int i = 0;
-        ListNode* ptr;
-        ListNode* ptr1;
-        ptr = head;
-        while(ptr!=NULL)
+    { 
+      int val1,val2,i = 0,length = size(head);
+        if(length == 1)
+            return head;
+        if(k == (length/2 + 1)&& length%2)
+            return head;
+        ListNode* curr = head;
+        ListNode *curr1 , *curr2;
+        while(curr)
         {
-            swappedlist.push_back(ptr->val);
-            ptr=ptr->next;
+            if(i+1 == k)
+            {
+                val1 = curr->val;
+                curr1 = curr;
+                
+            }
+            else if(i+1 == length - k + 1)
+            {
+                val2 = curr->val;
+                curr2 = curr;
+            }
+            curr = curr->next;
+            i++;
         }
-        int temp = swappedlist[k-1];
-        swappedlist[k-1] = swappedlist[swappedlist.size()-k];
-        swappedlist[swappedlist.size()-k]=temp;
-        ptr1 = head;
-        
-        while(ptr1!=NULL)
-        {
-            ptr1->val= swappedlist[i++];
-            ptr1=ptr1->next;
-        }
-     return head;
-        
-      
+        curr1->val = val2;
+        curr2->val = val1;
+        return head;
         
     }
 };
