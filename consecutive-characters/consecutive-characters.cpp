55796168645e2c5db68e70cf14e4a06d5d9715b9
SOHAM  SAHA  ROY <81096719+infuman69@users.noreply.github.com>
2021-12-14 01:33:48 +0530

    Time: 32 ms (5.28%), Space: 6.7 MB (71.89%) - LeetHub

---
+++ consecutive-characters/consecutive-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxPower(string s) 
+    {  
+        int i,x = 0,max = 0;
+        for( i = 0 ;i < s.size();i++)
+        {
+            if(s[i]==s[i+1])
+               x++;
+            else
+                x = 0;
+            if(max<x)
+                max = x;
+            cout<<x<<endl;
+        }
+        max++;
+        return max;
+    }
+};
