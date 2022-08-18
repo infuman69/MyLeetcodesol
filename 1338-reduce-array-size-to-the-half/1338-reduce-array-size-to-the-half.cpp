0a509d7af8d309025ea74db3bd2049b1e04d590a
SOHAM  SAHA  ROY <81096719+infuman69@users.noreply.github.com>
2022-08-19 01:14:48 +0530

    Time: 341 ms (51.85%), Space: 78.2 MB (79.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         int i,size = arr.size(),c =0,s = 0;
-        vector<int> freq;
+        priority_queue<int> freq;
         unordered_map<int,int> mp;
         for (auto x : arr)
             mp[x]++;
         for (auto i : mp)
-            freq.push_back(i.second);
+            freq.push(i.second);
         if(freq.size() == 1)
             return 1;
-        sort(freq.begin(),freq.end());
-        
-        for(i = freq.size() - 1;i >=0 ;i--)
-        {
+        while(!freq.empty()){
+            s += freq.top();
+            c++;
+            freq.pop();
             if(s >= size/2)
                 return c;
-            c++;
-            s += freq[i];
         }
-        
         return 0;
     }
 };
